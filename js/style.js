9d70b301f357d8bbfc78046fd5609951348dca5b
FaySie <sel12342001@gmail.com>
2016-08-15 16:33:59 +0800

    clothes img animate final

--- js/style.js
+++ js/style.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var wScroll = $(this).scrollTop();
-        console.log(wScroll); //可以測試滾動的垂直pixel
+        //console.log(wScroll); //可以測試滾動的垂直pixel
         $('.logo').css({
             'transform' : 'translate(0px,'+ wScroll /2 +'%)'
         });
+
         $('.back-bird').css({
             'transform' : 'translate(0px,'+ wScroll /4 +'%)'
         });
+
         $('.fore-bird').css({
             'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
         });
+
+        if(wScroll > $('.clothes').offset().top - ($(window).height()/1.2)){
+            //console.log('hi'); //測試畫面滑動到clothes的最上緣時說hi
+            $('.clothes figure').each(function(i){
+                setTimeout(function(){
+                    $('.clothes figure').eq(i).addClass('show');
+                }, 150 * (i+1)); //150s
+            });
+        }
     });
 });
