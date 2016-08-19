5db24ab30b633c57d3de7cab071e7f444b60f9a6
FaySie <sel12342001@gmail.com>
2016-08-19 11:25:00 +0800

    circle scroll & hover style final

--- js/style.js
+++ js/style.js
@@ -1,35 +1,43 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var wScroll = $(this).scrollTop();
-        //console.log(wScroll); //可以測試滾動的垂直pixel
+        // console.log(wScroll); //可以測試滾動的垂直pixel
         $('.logo').css({
-            'transform' : 'translate(0px,'+ wScroll /2 +'%)'
+            'transform' : 'translate(0px,'+ wScroll / 2 +'%)'
         });
 
         $('.back-bird').css({
-            'transform' : 'translate(0px,'+ wScroll /4 +'%)'
+            'transform' : 'translate(0px,'+ wScroll / 4 +'%)'
         });
 
         $('.fore-bird').css({
-            'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
+            'transform' : 'translate(0px, -'+ wScroll / 40 +'%)'
         });
 
-        if(wScroll > $('.clothes').offset().top - ($(window).height()/1.2)){
+        if(wScroll > $('.clothes').offset().top - ($(window).height() / 1.2)){
             //console.log('hi'); //測試畫面滑動到clothes的最上緣時說hi
             $('.clothes figure').each(function(i){
                 setTimeout(function(){
                     $('.clothes figure').eq(i).addClass('show');
-                }, 150 * (i+1)); //150s
+                }, 150 * (i + 1)); //150s
             });
         }
-    });
-    $('.img-row').hover(
-        function(){
-            $(this).children('figcaption').css('left','0%');
-            $(this).children('a').children('img').css('transform','scale(1.2)');
-        },function(){
-            $(this).children('figcaption').css('left','-100%');
-            $(this).children('a').children('img').css('transform','scale(1)');
+
+        $('.img-row').hover(
+            function(){
+                $(this).children('figcaption').css('left','0%');
+                $(this).children('a').children('img').css('transform','scale(1.2)');
+            },function(){
+                $(this).children('figcaption').css('left','-100%');
+                $(this).children('a').children('img').css('transform','scale(1)');
+            }
+        );
+
+        if(wScroll > $('.large-window').offset().top - $(window).height()){
+            $('.large-window').css({'background-position' : 'center ' + (wScroll - $('.large-window').offset().top) + 'px'});
+
+            var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+            $('.tint').css({'opacity' : opacity});
         }
-    );
+    });
 });
