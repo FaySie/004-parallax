fd6b43e9a4980ddc27383e465954851c8802e0a0
FaySie <sel12342001@gmail.com>
2016-08-22 14:14:41 +0800

    add footer

--- js/style.js
+++ js/style.js
@@ -39,5 +39,12 @@ $(document).ready(function(){
             var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
             $('.tint').css({'opacity' : opacity});
         }
+
+        if(wScroll > $('.blog').offset().top - $(window).height()){
+            var offset = Math.min(0,wScroll - $('.blog').offset().top + 100);
+            $('.post1').css({'transform' : 'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'});
+            $('.post3').css({'transform' : 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.2) +'px)'});
+
+        }
     });
 });
